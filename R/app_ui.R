755bafb90c26045c5dcc48b8024c791912f5fbc6
Culturology's Undead <a.pawluczenko@gmail.com>
2026-05-22 12:39:20 +0300

    reformat UI

--- R/app_ui.R
+++ R/app_ui.R
@@ -12,80 +12,79 @@ app_ui <- function(request) {
     i18n$set_translation_language("en")
     i18n$use_js()
 
-    # Wrap UI with cookie handlers
-    add_cookie_handlers(
-        tagList(
-            golem_add_external_resources(),
-            dashboardPage(title = "stancer playground",
-                          header = dashboardHeader(title = tagList(
-                              span(class = "logo-lg", "stancer playground"),
-                              icon("balance-scale")
-                          )),
+    tagList(
+        golem_add_external_resources(),
+        dashboardPage(title = "stancer playground",
+                      header = dashboardHeader(title = tagList(
+                          span(class = "logo-lg", "stancer playground"),
+                          icon("balance-scale")
+                      )),
 
-                          sidebar = dashboardSidebar(
-                              sidebarMenu(
-                                  id = "tabs",
-                                  menuItem(
-                                      i18n$t("Single Analysis"),
-                                      tabName = "single",
-                                      icon = icon("comment")
-                                  ),
-                                  menuItem(
-                                      i18n$t("Batch Analysis"),
-                                      tabName = "batch",
-                                      icon = icon("file-csv")
-                                  ),
-                                  menuItem(
-                                      i18n$t("Prompt Editor"),
-                                      tabName = "prompts",
-                                      icon = icon("edit")
-                                  ),
-                                  menuItem(
-                                      i18n$t("Settings"),
-                                      tabName = "settings",
-                                      icon = icon("cog")
-                                  )
+                      sidebar = dashboardSidebar(
+                          sidebarMenu(
+                              id = "tabs",
+                              menuItem(
+                                  i18n$t("Single Analysis"),
+                                  tabName = "single",
+                                  icon = icon("comment")
+                              ),
+                              menuItem(
+                                  i18n$t("Batch Analysis"),
+                                  tabName = "batch",
+                                  icon = icon("file-csv")
+                              ),
+                              menuItem(
+                                  i18n$t("Prompt Editor"),
+                                  tabName = "prompts",
+                                  icon = icon("edit")
+                              ),
+                              menuItem(
+                                  i18n$t("Settings"),
+                                  tabName = "settings",
+                                  icon = icon("cog")
                               )
-                          ),
-
-                          controlbar = dashboardControlbar(
-                              id = "controlbar",
-                              width = 300,
-                              mod_model_config_ui("model_config_1", i18n)
-                          ),
-
-                          body = dashboardBody(
-                              tabItems(
-                                  # Single Analysis Tab
-                                  tabItem(
-                                      tabName = "single",
-                                      mod_single_analysis_ui("single_analysis_1", i18n)
-                                  ),
-
-                                  # Batch Analysis Tab
-                                  tabItem(
-                                      tabName = "batch",
-                                      mod_batch_analysis_ui("batch_analysis_1", i18n)
-                                  ),
-
-                                  # Placeholder tabs
-                                  tabItem(
-                                      tabName = "prompts",
-                                      h2("Prompt Editor (Coming Soon)")
-                                  ),
-                                  tabItem(
-                                      tabName = "settings",
-                                      mod_settings_ui("settings_1", i18n)
-                                  )
-                              )
-                          ),
-                          footer = dashboardFooter(
-                              left = "Built with {stancer} and {golem}",
-                              right = "2026"
                           )
-            )
+                      ),
+
+                      controlbar = dashboardControlbar(
+                          id = "controlbar",
+                          width = 300,
+                          mod_model_config_ui("model_config_1", i18n)
+                      ),
+
+                      body = dashboardBody(
+                          tabItems(
+                              # Single Analysis Tab
+                              tabItem(
+                                  tabName = "single",
+                                  mod_single_analysis_ui("single_analysis_1", i18n)
+                              ),
+
+                              # Batch Analysis Tab
+                              tabItem(
+                                  tabName = "batch",
+                                  mod_batch_analysis_ui("batch_analysis_1", i18n)
+                              ),
+
+                              # Placeholder tabs
+                              tabItem(
+                                  tabName = "prompts",
+                                  h2("Prompt Editor (Coming Soon)")
+                              ),
+                              tabItem(
+                                  tabName = "settings",
+                                  mod_settings_ui("settings_1", i18n)
+                              )
+                          )
+                      ),
+                      footer = dashboardFooter(
+                          left = "Built with {stancer} and {golem}",
+                          right = "2026"
+                      )
         )
-    )
+    ) |>
+        # Wrap UI with cookie handlers
+        add_cookie_handlers()
 }
 
 golem_add_external_resources <- function() {
