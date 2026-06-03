b8c51b64ce263462b89ea6a5e2c787d68189dcbf
Culturology's Undead <a.pawluczenko@gmail.com>
2026-06-03 15:27:10 +0300

    Chat creation helpers

--- R/utils-server.R
+++ R/utils-server.R
@@ -1,1 +1,18 @@
 `%||%` <- function(x, y) if (is.null(x)) y else x
+
+api_key <- function(key) {
+    function() {
+        list(Authorization = paste(
+            'Bearer', key
+        ))
+    }
+}
+
+prepare_chat <- function(config, params = NULL) {
+    ellmer::chat(
+        name = paste(config$provider, config$model, sep = "/"),
+        credentials = api_key(config$key),
+        echo = "none",
+        params = params
+    )
+}
