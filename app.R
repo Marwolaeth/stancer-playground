4206ba4f6aaef1a48dece0a3a01f4c4d35931dfa
Andriej Pawluczenko <you@example.com>
2026-07-01 09:59:29 +0000

    reformat

--- app.R
+++ app.R
@@ -4,6 +4,6 @@
 
 remotes::install_github("Marwolaeth/stancer-playground")
 usethis::use_git_ignore("rsconnect/")
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+options("golem.app.prod" = TRUE)
 stancer.playground::run_app() # add parameters here (if any)
